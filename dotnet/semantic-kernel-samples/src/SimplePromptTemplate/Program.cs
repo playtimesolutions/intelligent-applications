2a69dc51c5491da50b1bcfbca0601f0a1c50418b
Adrian Watson <adrian.watson@playtimesolutions.com.au>
2024-11-15 13:13:30 +1100

    Added prompt-template sample

---
+++ dotnet/semantic-kernel-samples/src/SimplePromptTemplate/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.Extensions.Configuration;
+
+using SimplePromptTemplate;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{Environment.UserName}.json", optional: true)
+    .Build();
+var appConfiguration = configuration.Get<ApplicationConfiguration>()!;
+
+IKernelBuilder kernelBuilder = Kernel
+    .CreateBuilder()
+    .AddAzureOpenAIChatCompletion(appConfiguration.DeploymentName, appConfiguration.Endpoint, appConfiguration.ApiKey);
+Kernel kernel = kernelBuilder.Build();
+
+const string prompt = "What is the timezone at {{$location}}?";
+
+string? userInput;
+Console.Write("User enter a location > ");
+userInput = Console.ReadLine();
+
+if (string.IsNullOrEmpty(userInput))
+{
+    return;
+}
+
+KernelFunction kernelFunction = kernel.CreateFunctionFromPrompt(prompt);
+FunctionResult result = await kernelFunction.InvokeAsync(kernel, new() { ["location"] = userInput });
+
+Console.WriteLine("Assistant > " + result);
